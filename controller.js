17fa3e262f4a839db0d3b287dabd626b6e4e01ed
Jeremy Block <j.block@ufl.edu>
2023-05-05 10:34:40 -0400

    Minor variable Renames. Push from Webstorm

--- controller.js
+++ controller.js
@@ -1,11 +1,11 @@
 const reader = require("./modules/filereader");
 const saveJsonToCsvFile = require("./modules/saveJsonToCsvFile")
 
-let outputfilename = "testing"
+let outputFileName = "testing"
 
 const jsonData = reader(
   "data/Dataset_1/User Interactions/Arms_P1_InteractionsLogs.json"
 );
 console.log(jsonData)
 
-saveJsonToCsvFile(outputfilename, jsonData);
+saveJsonToCsvFile(outputFileName, jsonData);
