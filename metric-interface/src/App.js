2eadde2a3211feced1df628650e4eecd5fa101aa
Jeremy Block <j.block@ufl.edu>
2023-07-03 23:39:20 -0400

    Basic Bar Chart Displays now.

--- metric-interface/src/App.js
+++ metric-interface/src/App.js
@@ -1,23 +1,35 @@
 // src/App.js
 import Vizzu from "vizzu";
-import VizzuModule from 'vizzu/dist/cvizzu.wasm';
+import VizzuModule from "vizzu/dist/cvizzu.wasm";
 import { data } from "./demoData";
+import { useRef, useEffect } from "react";
 
 Vizzu.options({ wasmUrl: VizzuModule });
 
 function App() {
-  const chart = new Vizzu("myVizzu", { data });
-  chart.initializing.then((chart) =>
-    chart.animate({
-      config: {
-        channels: {
-          y: { set: ["Popularity"] },
-          x: { set: ["Genres"] },
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const chart = new Vizzu(canvasRef.current, { data });
+    chart.initializing.then((chart) =>
+      chart.animate({
+        config: {
+          channels: {
+            y: { set: ["Popularity"] },
+            x: { set: ["Genres"] },
+          },
         },
-      },
-    })
+      })
+    );
+  }, []);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      id="myVizzu"
+      style={{ width: "800px", height: "480px" }}
+    />
   );
-  return <canvas id="myVizzu" style={{ width: "800px", height: "480px" }} />;
 }
 
 export default App;
